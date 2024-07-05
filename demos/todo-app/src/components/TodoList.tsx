5d90d11d55303fef9cbbc75eaa5e606d487cf301
jianjustin <janine.jian.chen@gmail.com>
2024-07-05 14:18:03 +0800

    feat: add react-app

--- demos/todo-app/src/components/TodoList.tsx
+++
@@ -1,7 +0,0 @@
-import { TodoItem } from './TodoItem'
-import { useTodo } from '../context'
-import { SiStarship } from 'react-icons/si'
-
-export const TodoList = () => {
-  return <div>TodoList</div>
-}
--- demos/todo-app/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { AddTodo } from './AddTodo'
-export { Input } from './Input'
-export { TodoItem } from './TodoItem'
-export { TodoList } from './TodoList'
