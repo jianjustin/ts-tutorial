4139cd9c97cd4e872ed1a0b5d64a0d9b66f0bf5d
chenjianrui <chenjianrui@motern.com>
2024-07-08 22:22:12 +0800

    feat: add vite app

---
+++ tutorial/src/tutorial.ts
@@ -0,0 +1,27 @@
+console.log("app")
+
+let greeting:string = "hello typescript"
+greeting = greeting.toUpperCase()
+console.log(greeting)
+
+let age: number = 25
+age = age + 5
+console.log(`age=${age}`)
+
+let isAdult: boolean = age>=18
+console.log(`isAdult=${isAdult}`)
+
+let tax: number|string = 10
+tax = "money"
+console.log(`tax=${tax}`)
+
+const books = ["book1", 'book2', 'book3']
+let foundbook: string | undefined
+for (let book of books) {
+    if (book == 'book1'){
+        foundbook = book
+        console.log(`book found! is ${foundbook.toUpperCase()}`)
+        break
+    }
+}
+console.log(`found book ${foundbook?.toLowerCase()}`)
